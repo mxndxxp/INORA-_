c8ba93f3c7f2259afd78c0e7a65e15a9e6f8199b
Mandeep Ghosh <mandyxxxghoshxxx@gmail.com>
2025-09-12 12:48:29 +0000

    i told u not to add on the water animation i told u to add on the backgr

--- src/components/raindrop-animation.tsx
+++ src/components/raindrop-animation.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export function RaindropAnimation() {
   return (
-    <div className="raindrop-container absolute inset-0 w-full h-full -z-10 overflow-hidden">
+    <div className="raindrop-container absolute inset-0 w-full h-full -z-20 overflow-hidden">
       {[...Array(9)].map((_, i) => (
         <div key={i} className="rain">
           <div className="drop" />
