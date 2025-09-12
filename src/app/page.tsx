c910d43147311aee63aa09a4af408a500a17576c
Mandeep Ghosh <mandyxxxghoshxxx@gmail.com>
2025-09-12 13:08:46 +0000

    where is the water aniamtion add it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-[100dvh]">
       <section className="relative w-full min-h-[90vh] md:min-h-screen flex items-center justify-center overflow-hidden">
-        
+        <WaterAnimation />
         {/* Left Floating Brands */}
         <div className="absolute top-1/2 -translate-y-1/2 left-4 md:left-8 lg:left-16 flex-col gap-6 z-30 flex">
           {leftBrands.map((brand, index) => {
